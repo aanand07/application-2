0550e965e71afd3c24b0ceabf5235fd9c71d7967
Anand Bhushan <abhushan@corp.lpl.com>
2018-03-01 16:37:07 +0530

    added method for api call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 import { HttpClient } from '@angular/common/http';
-import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/map';
+import { weatherModelComponent } from './weatherModel.component';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -10,32 +12,42 @@ import 'rxjs/add/operator/map'
 })
 export class AppComponent implements OnInit {
   location = {};
-  check:boolean;
-  weatherData :any;
-  constructor(private http: HttpClient){}
-  setPosition(position){
+  check: boolean;
+
+  model: weatherModelComponent;
+  constructor(private http: HttpClient) { }
+  setPosition(position) {
     console.log(position.coords);
-     this.location = position.coords;
-     console.log(this.location);
-     }
-    posError(error){
-      if(error.code===1){
-      this.check=true; 
+    this.getWeatherOnLonApiCall(position.coords.latitude, position.coords.longitude)
+      .subscribe(response => { this.model = response; console.log(this.model); alert(this.model.id) });
+  }
+  posError(error) {
+    if (error.code === 1) {
+      this.check = true;
       console.log(error.message);
-      }
     }
-    getWeatherOncoun(country, zip)
-    {
-      alert("success");
-      let uri="http://openweathermap.org/data/2.5/weather?zip="+zip+","+country+"&appid=b6907d289e10d714a6e88b30761fae22";
-      this.http.get(uri
-        ).map(response => response).subscribe(response=>{this.weatherData=response;console.log(this.weatherData);})
+  }
+  //user allows
+  getWeatherOnLonApiCall(lat, lon): Observable<weatherModelComponent> {
+    let uri = "http://openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=b6907d289e10d714a6e88b30761fae22";
+    return this.http.get(uri)
+      .map(response => { return response as weatherModelComponent })
+  }
+  //user blocks
+  getWeatherOncoun(country, zip) {
+
+    this.getWeatherOnCounApiCall(country, zip).subscribe(response => { this.model = response; console.log(this.model); alert(this.model.id) });
+  }
+  getWeatherOnCounApiCall(country, zip): Observable<weatherModelComponent> {
+    let uri = "http://openweathermap.org/data/2.5/weather?zip=" + zip + "," + country + "&appid=b6907d289e10d714a6e88b30761fae22";
+    return this.http.get(uri)
+      .map(response => { return response as weatherModelComponent })
+  }
+
+  ngOnInit() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.setPosition.bind(this), this.posError.bind(this));
+
     }
-    
-ngOnInit(){
-  if(navigator.geolocation){
-     navigator.geolocation.getCurrentPosition(this.setPosition.bind(this), this.posError.bind(this));
-     
-     };
   }
 }
---
+++ src/app/weatherModel.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+
+})
+export class weatherModelComponent{
+  constructor(){}
+  coord :any;
+  weather :any;
+  base: any;
+  main : any;
+  visibility :any;
+  wind: any;
+  clouds :any;
+  dt:any;
+  sys:any;
+  id:any;
+  name:any;
+  cod:any;
+}
